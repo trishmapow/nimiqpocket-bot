e1fabc6de59ab8462446b7844e0abc9445008aef
Christopher Malau <chrismalau1@gmail.com>
2018-08-06 18:45:39 +1000

    Fix not looping

--- bot.py
+++ bot.py
@@ -35,45 +35,50 @@ def main():
     async def background_update():
         global num_blocks
         global pool_msg
-        num_blocks_cur = -1
-        sum_hr = 0
-        sum_clients = 0
-        for server in servers:
-            try:
-                req = requests.get("https://{}.nimiqpocket.com:8444/api/poolstats".format(server), timeout=5)
-            except requests.Timeout:
-                print("Couldn't connect to {}".format(server))
-                break
-
-            json = req.json()
-            servers[server]["hr"] = json["hashRate"]
-            sum_hr += int(json["hashRate"])
-            servers[server]["clients"] = json["numClients"]
-            sum_clients += int(json["numClients"])
-
-            if server == "hk":
-                num_blocks_cur = json["minedBlocks"]
-                pool_fee = json["poolFee"]
 
         await client.wait_until_ready()
         channel = discord.Object(id=BLOCKS_CHANNEL)
 
-        print(channel)
-        if num_blocks_cur != num_blocks:
-            num_blocks = num_blocks_cur
-            if channel is not None:
-                msg = "`We found Nimiqpocket's #{} block!` :tada:".format(num_blocks_cur)
-                await client.send_message(channel, msg)
+        while not client.is_closed:
+            num_blocks_cur = -1
+            sum_hr = 0
+            sum_clients = 0
 
-        arr = []
-        for server in servers:
-            arr.append([server, servers[server]["hr"], servers[server]["clients"]])
-        arr.append(["TOTAL", str(round(float(sum_hr)/1e6, 2))+"MH/s", sum_clients])
-        arr.append(["","",""])
-        arr.append(["","Fee "+str(pool_fee)+"%","Blocks: "+str(num_blocks_cur)])
-        pool_msg = tabulate(arr, headers=["Server", "Hashrate", "# miners"])
+            for server in servers:
+                try:
+                    req = requests.get("https://{}.nimiqpocket.com:8444/api/poolstats".format(server), timeout=5)
+                except requests.Timeout:
+                    print("Couldn't connect to {}".format(server))
+                    break
 
-        await asyncio.sleep(60)
+                json = req.json()
+                servers[server]["hr"] = json["hashRate"]
+                sum_hr += int(json["hashRate"])
+                servers[server]["clients"] = json["numClients"]
+                sum_clients += int(json["numClients"])
+
+                if server == "hk":
+                    num_blocks_cur = json["minedBlocks"]
+                    pool_fee = json["poolFee"]
+
+            print(channel)
+            if num_blocks_cur != num_blocks:
+                num_blocks = num_blocks_cur
+                if channel is not None:
+                    msg = "`We found Nimiqpocket's #{} block!` :tada:".format(num_blocks_cur)
+                    await client.send_message(channel, msg)
+
+            arr = []
+            for server in servers:
+                arr.append([server, servers[server]["hr"], servers[server]["clients"]])
+
+            print(servers)
+            arr.append(["TOTAL", str(round(float(sum_hr)/1e6, 2))+"MH/s", sum_clients])
+            arr.append(["","",""])
+            arr.append(["","Fee "+str(pool_fee)+"%","Blocks: "+str(num_blocks_cur)])
+            pool_msg = tabulate(arr, headers=["Server", "Hashrate", "# miners"])
+
+            await asyncio.sleep(30)
 
     client.loop.create_task(background_update())
     client.run(BOT_TOKEN)
