ec7ebb8cf7097d464a9eb75aaf72f50174cefac5
Christopher Malau <chrismalau1@gmail.com>
2018-08-06 11:47:48 +1000

    Bugfix + add fee/miner info

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ servers = {"hk": {},"eu": {},"pool": {}}
 num_blocks = -1
 pool_fee = -1
 channel = None
+pool_msg = ""
 
 def main():
     client = discord.Client()
@@ -29,14 +30,14 @@ def main():
     @client.event
     async def on_message(message):
         if message.content.startswith("!pool"):
-            arr = []
-            for server in servers:
-                arr.append([server, servers[server]["hr"], servers[server]["clients"]])
-            msg = tabulate(arr, headers=["Server", "Hashrate", "# miners"])
-            await client.send_message(message.channel, "```js\n{}```".format(msg))
+            await client.send_message(message.channel, "```js\n{}```".format(pool_msg))
 
     async def background_update():
+        global num_blocks
+        global pool_msg
         num_blocks_cur = -1
+        sum_hr = 0
+        sum_clients = 0
         for server in servers:
             try:
                 req = requests.get("https://{}.nimiqpocket.com:8444/api/poolstats".format(server), timeout=5)
@@ -46,7 +47,10 @@ def main():
 
             json = req.json()
             servers[server]["hr"] = json["hashRate"]
+            sum_hr += int(json["hashRate"])
             servers[server]["clients"] = json["numClients"]
+            sum_clients += int(json["numClients"])
+
             if server == "hk":
                 num_blocks_cur = json["minedBlocks"]
                 pool_fee = json["poolFee"]
@@ -56,9 +60,18 @@ def main():
 
         print(channel)
         if num_blocks_cur != num_blocks:
+            num_blocks = num_blocks_cur
             if channel is not None:
                 msg = "`We found Nimiqpocket's #{} block!` :tada:".format(num_blocks_cur)
                 await client.send_message(channel, msg)
+
+        arr = []
+        for server in servers:
+            arr.append([server, servers[server]["hr"], servers[server]["clients"]])
+        arr.append(["TOTAL", str(round(float(sum_hr)/1e6, 2))+"MH/s", sum_clients])
+        arr.append(["","",""])
+        arr.append(["","Fee "+str(pool_fee)+"%","Blocks: "+str(num_blocks_cur)])
+        pool_msg = tabulate(arr, headers=["Server", "Hashrate", "# miners"])
 
         await asyncio.sleep(60)
 
@@ -70,4 +83,4 @@ if __name__ == "__main__":
         try:
             main()
         except ConnectionResetError:
-            sleep(5)
+            sleep(10)
